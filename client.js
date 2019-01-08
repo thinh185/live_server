5e87e60ced755e48a490b3d28200aec4ed7c4090
thinhnn3 <thinhnn3@cyberspace.vn>
2019-01-08 19:38:21 +0700

    init server

---
+++ client.js
@@ -0,0 +1,7 @@
+const io = require("socket.io-client"),
+  ioClient = io.connect("http://localhost:3333");
+ioClient.emit('testconnect', {data: "data"})
+
+ioClient.on("testconnect", (msg) => {
+  console.log(msg)
+});
---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'server',
+  script: './server.js',
+  watch: false,
+  env_sit: {
+    NODE_ENV: 'sit'
+  }
+};
